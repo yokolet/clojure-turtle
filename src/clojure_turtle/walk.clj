cf089ef420226eb4e0048fa7c3f36efe3d305eac
Yoko Harada <yokolet@gmail.com>
2015-07-08 22:41:59 -0400

    Adds practice base code

---
+++ src/clojure_turtle/walk.clj
@@ -0,0 +1,20 @@
+(ns clojure-turtle.walk
+  (:use clojure-turtle.core))
+(clean)
+(home)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; add some functions under these comment lines
+;;
+;; for example
+;; (forward 30)
+;; (right 90)
+;; (forward 30)
+;; (right 90)
+;;
+;; evaluate code by hitting cmd/ctrl + shift + enter
+;;
+;; see how turtle walks
+;;
+
+
